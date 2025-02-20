0bc37b022934358363b521857f55a00d26d28212
sainitin18 <124301819+sainitin18@users.noreply.github.com>
2025-02-20 17:35:06 +0530

    Add files via upload

---
+++ Task_UnitTesting/VoterValidator.java
@@ -0,0 +1,14 @@
+package Task_UnitTesting;
+
+public class VoterValidator {
+	public boolean validateVoterAge(int age) throws Exception {
+		if(age<0)
+			throw new Exception("Invalid age");
+		if(age >= 18) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
